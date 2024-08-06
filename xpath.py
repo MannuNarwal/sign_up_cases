5cce48c2af9e1949a18ac7a2d249bce9f8bfa15d
Manisha Narwal <narwalmanish1@gmail.com>
2024-08-06 11:33:55 +0530

    xpaths added

--- xpath.py
+++ xpath.py
@@ -1,7 +1,73 @@
+
+#Signup Page
 signup_url="https://zouk.co.in/account/register"
 first_name="input-template--22762731569448__main--customerfirst_name"
 last_name="input-template--22762731569448__main--customerlast_name"
 signup_email="input-template--22762731569448__main--customeremail"
 s_password="input-template--22762731569448__main--customerpassword"
-s_submit="/html/body/main/section/div/div/div/div/div[3]/form/button"
-home_page="https://zouk.co.in"
+z_page="https://zouk.co.in"
+click="/html/body/main/section/div/div/div/div/div[3]/form/button" 
+
+
+# login page
+login_url="https://zouk.co.in/account/login?return_url=%2Faccount"
+login_button ="/html/body/main/section/div/div/account-login/div[1]/div[3]/form/button"
+Home_page="https://zouk.co.in/account/login?return_url=%2Faccount"
+login_id="input-template--22965915648296__main--customeremail"
+login_password= "input-template--22965915648296__main--customerpassword"
+dashboard= "https://zouk.co.in/account"
+click_forget_password= "/html/body/main/section/div/div/account-login/div[1]/div[3]/form/div[1]/div[2]/div[2]/a"
+recover_password= "https://zouk.co.in/account/login?return_url=%2Faccount#recover"
+
+
+# bag page
+bag_button="/html/body/header/height-observer/x-header/nav[1]/ul/li[1]/details/summary" 
+bag_page="https://zouk.co.in/collections/bags?ref=nav-top"
+bag_filter='/html/body/facets-drawer/form/div/details[2]/summary/span/span'
+view_result="/html/body/facets-drawer/div/button"
+sort_button="/html/body/main/section[3]/div/div/height-observer/div/div[2]/button/span"
+
+# filter options
+price_tab="/html/body/facets-drawer/form/div/details[2]/summary/span"
+product_tab="/html/body/facets-drawer/form/div/details[3]/summary/span"
+pattern_tab="/html/body/facets-drawer/form/div/details[6]/summary/span/span"
+occation_tab="/html/body/facets-drawer/form/div/details[8]/summary/span/span"
+category_tab="/html/body/facets-drawer/form/div/details[4]/summary/span/span"
+color_tab="/html/body/facets-drawer/form/div/details[5]/summary/span/span"
+
+# selected filter
+price1="/html/body/facets-drawer/form/div/details[2]/div/price-range/div[2]/label[1]/input"
+price2= "/html/body/facets-drawer/form/div/details[2]/div/price-range/div[2]/label[2]/input"
+product="/html/body/facets-drawer/form/div/details[3]/div/div/label[1]"
+pattern="/html/body/facets-drawer/form/div/details[6]/div/div/label[1]"
+occation="/html/body/facets-drawer/form/div/details[8]/div/div/label"
+category="/html/body/facets-drawer/form/div/details[4]/div/div/label"
+color="/html/body/facets-drawer/form/div/details[5]/div/div/label"
+
+# products listed
+price_products="/html/body/main/section[3]/div/div/div/div/div/div[2]/span"
+products_products="/html/body/main/section[3]/div/div/div/div/div/div[3]/span"
+pattern_products="/html/body/main/section[3]/div/div/div/div/div/div[4]/span"
+occation_products="/html/body/main/section[3]/div/div/div/div/div/div[5]/span"
+category_products="/html/body/main/section[3]/div/div/div/div/div/div[4]/span"
+color_products="/html/body/main/section[3]/div/div/div/div/div/div[5]/span"
+
+#  product sorted
+Alphabetically_sort="/html/body/main/section[3]/div/div/height-observer/div/div[2]/facets-sort-popover/x-listbox/button[3]/span"
+price_sort="/html/body/main/section[3]/div/div/height-observer/div/div[2]/facets-sort-popover/x-listbox/button[5]/span"
+date_sort="/html/body/main/section[3]/div/div/height-observer/div/div[2]/facets-sort-popover/x-listbox/button[7]/span"
+alp_products="/html/body/main/section[3]/div/div/div/div/product-list/product-card[1]/div[3]/div/div/a"
+low="/html/body/main/section[3]/div/div/div/div/product-list/product-card[1]/div[3]/div/div/price-list/sale-price"
+old="/html/body/main/section[3]/div/div/div/div/product-list/product-card[2]/div[3]/div/div/a"
+
+clear_tab="/html/body/main/section[3]/div/div/div/div/div/a/span"
+add_wishlist="/html/body/main/section[3]/div/div/div/div/product-list/product-card[3]/div[1]/div/div/button/svg"
+wishlist_tab="/html/body/header/height-observer/x-header/nav[2]/a[2]/svg"
+wishlisted="/html/body/div[17]/div[3]/div/div[2]/p/div/div/div[1]/div/div/p/div/div/span[1]/a"
+remove_wishlist="/html/body/div[18]/div[3]/div/div[2]/p/div/div/div[2]/button[2]/span[1]"
+ok_click="/html/body/div[19]/div[3]/div/div[3]/button[2]/span[1]"
+empty_wishlist="/html/body/div[18]/div[3]/div/div[2]/p/div/div/div/div"
+cart_product="/html/body/main/section[3]/div/div/div/div/product-list/product-card[2]/div[2]/form/button"
+cart="/html/body/cart-drawer/p"
+remove_cart="/html/body/cart-drawer/div[1]/line-item/div/div/line-item-quantity/a"
+empty_cart="/html/body/cart-drawer/div[1]/line-item/div/div/line-item-quantity/a"
